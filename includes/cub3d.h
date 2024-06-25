3976856e8c1570c6b92a8d8974aabc5c8af7f665
Julie Labonde <jlabonde@made-f0Ar9s6.clusters.42paris.fr>
2024-06-25 12:06:19 +0200

    added window init with mlx functions + esc and red cross handling

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -4,5 +4,14 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <mlx.h>
+
+typedef struct s_cub
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_cub;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
 #include "cub3d.h"
 
+int	destroyer(t_cub *cub)
+{
+	mlx_destroy_window(cub->mlx_ptr, cub->win_ptr);
+	mlx_destroy_display(cub->mlx_ptr);
+	free(cub->mlx_ptr);
+	exit(0);
+	return (0);
+}
+
+int	key_press(int keycode, t_cub *cub)
+{
+	if (keycode == XK_Escape)
+		destroyer(cub);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	printf("Hello, World!\n");
+	t_cub cub;
+	cub.mlx_ptr = mlx_init();
+	if (!cub.mlx_ptr)
+		return (1);
+	cub.win_ptr = mlx_new_window(cub.mlx_ptr, 800, 600, "cub3d"); // replace 800 and 600 with the calculated win height and length
+	if (!cub.win_ptr)
+		return (free(cub.mlx_ptr), 1);
+	mlx_hook(cub.win_ptr, 17, 1L << 17, &destroyer, &cub);
+	mlx_hook(cub.win_ptr, KeyRelease, KeyReleaseMask, &key_press, &cub);
+	mlx_loop(cub.mlx_ptr);
 	return (0);
 }
