1ad8e7dda25e5b9bdde5e9b6005546d064dd28ba
Alisa Tonkopiy <atonkopi@made-f0Ar4s5.clusters.42paris.fr>
2024-07-02 13:09:21 +0200

    fixed mem errors in colors parsing

--- src/parsing_colors.c
+++ src/parsing_colors.c
@@ -25,14 +25,14 @@ int	get_rgb_int(char **colors)
 	return ((int_colors[0] << 16) | (int_colors[1] << 8) | int_colors[2]);
 }
 
-void	set_color_value(char *line, char **colors, char **nodes, t_cub *cub)
+void	set_color_value(char *line, char **colors, char **color_ids, t_cub *cub)
 {
 	int	color;
 
-	if ((cub->textures.ceiling_color > 0 && line[0] == 'C')
-		|| (cub->textures.floor_color > 0 && line[0] == 'F'))
+	if ((cub->textures.ceiling_color >= 0 && line[0] == 'C')
+		|| (cub->textures.floor_color >= 0 && line[0] == 'F'))
 	{
-		free_array(nodes);
+		free_array(color_ids);
 		free_array(colors);
 		free(line);
 		clean_up(cub, "Duplicate color definitions found");
@@ -40,7 +40,7 @@ void	set_color_value(char *line, char **colors, char **nodes, t_cub *cub)
 	color = get_rgb_int(colors);
 	if (color == -1)
 	{
-		free_array(nodes);
+		free_array(color_ids);
 		free_array(colors);
 		free(line);
 		clean_up(cub, "Invalid color value");
@@ -51,14 +51,14 @@ void	set_color_value(char *line, char **colors, char **nodes, t_cub *cub)
 		cub->textures.ceiling_color = color;
 	else
 	{
-		free_array(nodes);
+		free_array(color_ids);
 		free_array(colors);
 		clean_up(cub, "Invalid color identifier");
 	}
-	// free_array(colors);
+	free_array(colors);
 }
 
-void	parse_color(char *line, t_cub *cub, char** color_ids)
+void	parse_color(char *line, t_cub *cub, char **color_ids)
 {
 	char	**nodes;
 	char	**colors;
@@ -67,55 +67,37 @@ void	parse_color(char *line, t_cub *cub, char** color_ids)
 
 	i = 0;
 	if (invalid_commas(line))
-	{
-		free(line);
-		free_array(color_ids);
-		clean_up(cub, "Invalid color declaration");
-	}
+		free(line), free_array(color_ids), clean_up(cub,
+			"Invalid color declaration");
 	temp = ft_strtrim(line, "FC");
 	nodes = ft_split(temp, ',');
 	free(temp);
 	if (!nodes)
-	{
-		free(line);
-		free_array(color_ids);
-		clean_up(cub, "Memory allocation failed");
-	}
+		free(line), free_array(color_ids), clean_up(cub,
+			"Memory allocation failed");
 	if (array_len(nodes) != 3)
-	{
-		free_array(nodes);
-		free_array(color_ids);
-		free(line);
-		clean_up(cub, "Invalid color declaration");
-	}
+		free_array(nodes), free_array(color_ids), free(line), clean_up(cub,
+			"Invalid color declaration");
 	colors = ft_calloc(sizeof(char *) * array_len(nodes) + 1, sizeof(char *));
 	if (!colors)
-	{
-		free_array(color_ids);
-		clean_up(cub, "Memory allocation failed");
-	}
+		free_array(color_ids), clean_up(cub, "Memory allocation failed");
 	while (nodes[i])
 	{
 		temp = ft_strtrim(nodes[i], SPACES);
 		colors[i] = ft_strdup(temp);
 		if (!colors[i])
-		{
-			free_array(nodes);
-			free_array(color_ids);
-			clean_up(cub, "Memory allocation failed");
-		}
+			free_array(nodes), free_array(color_ids), clean_up(cub,
+				"Memory allocation failed");
 		free(temp);
 		i++;
 	}
-	set_color_value(line, colors, nodes, cub);
-	free_array(colors);
 	free_array(nodes);
+	set_color_value(line, colors, color_ids, cub);
 	free(line);
 }
 void	colors_errors_check(t_cub *cub)
 {
-	//change the condition here, because color can be 0
-	if (cub->textures.floor_color <= 0 || cub->textures.ceiling_color <= 0)
+	if (cub->textures.floor_color < 0 || cub->textures.ceiling_color < 0)
 		clean_up(cub, "Missing color(s) definition");
 	return ;
 }
